f956bcf33d94827c3f35b82b99b15dfc23f9c2ed
JobairulHassan <hasanjobairul@gmail.com>
2022-02-11 04:06:33 +0600

    Shelf Management updated

--- src/grab/go/Shelf_MngController.java
+++ src/grab/go/Shelf_MngController.java
@@ -5,23 +5,150 @@
  */
 package grab.go;
 
+import DatabaseConnection.DBconnection;
+import static grab.go.ShowEmployeeController.empn;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
  *
- * @author Asus
+ * @author FUJITSU
  */
 public class Shelf_MngController implements Initializable {
+
+    @FXML
+    private TableView<ShelfManage> Shelf_mngTable;
+    @FXML
+    private TableColumn<ShelfManage, String> Shelf_productID;
+    @FXML
+    private TableColumn<ShelfManage, String> Shelf_productName;
+    @FXML
+    private TableColumn<ShelfManage, String> Shelf_blockNum;
+    @FXML
+    private TableColumn<ShelfManage, String> Shelf_shelfNum;
+    @FXML
+    private TableColumn<ShelfManage, Integer> Shelf_row;
+    @FXML
+    private TableColumn<ShelfManage, Integer> Shelf_col;
+    @FXML
+    private TableColumn<ShelfManage, Integer> Shelf_productQty;
+    @FXML
+    private TableColumn<ShelfManage, String> Shelf_stockCon;
+    @FXML
+    private TableColumn<ShelfManage, String> Shelf_addSts;
+    
+    static ObservableList<ShelfManage> Shelf_productMngList=FXCollections.observableArrayList();
+    @FXML
+    private AnchorPane tableAnc;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        Shelf_mngTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        
+        try {
+            Shelf_productMngList=getAllShelfProduct();
+        } catch (SQLException ex) {
+            Logger.getLogger(ShowEmployeeController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ShowEmployeeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
+        Shelf_productID.setCellValueFactory(new PropertyValueFactory<ShelfManage, String>("Product_Id"));
+        Shelf_productName.setCellValueFactory(new PropertyValueFactory<ShelfManage, String>("Product_Name"));
+        Shelf_blockNum.setCellValueFactory(new PropertyValueFactory<ShelfManage, String>("Block_no"));
+        Shelf_shelfNum.setCellValueFactory(new PropertyValueFactory<ShelfManage, String>("Shelf_no"));
+        Shelf_row.setCellValueFactory(new PropertyValueFactory<ShelfManage, Integer>("Shelf_row"));
+        Shelf_col.setCellValueFactory(new PropertyValueFactory<ShelfManage, Integer>("Shelf_col"));
+        Shelf_productQty.setCellValueFactory(new PropertyValueFactory<ShelfManage, Integer>("OnShelf_quantity"));
+        Shelf_stockCon.setCellValueFactory(new PropertyValueFactory<ShelfManage, String>("Stock_Con"));
+        Shelf_addSts.setCellValueFactory(new PropertyValueFactory<ShelfManage, String>("Add_sts"));
+        
+        Shelf_mngTable.setItems(Shelf_productMngList);
+    } 
     
+    public  ObservableList<ShelfManage> getAllShelfProduct() throws SQLException, ClassNotFoundException{
+         ObservableList<ShelfManage> ShelfProductList=FXCollections.observableArrayList();
+         DBconnection dbc =new DBconnection();
+         dbc.connectToDB();
+         String query= "SELECT  Products.ProductID,ProductName,ShelfNo,Block_shelfNo,Shelf_row,Shelf_col,OnShelf_qty,Stock_condition,Add_status FROM Products LEFT JOIN Shelf on Products.ProductID=Shelf.ProductID"; 
+         dbc.queryToDB(query);
+         ResultSet rs= dbc.queryToDB(query);
+        
+        while(rs.next()){
+        String ProductID=rs.getString("ProductID");
+        String ProductName=rs.getString("ProductName");
+        String Block_no=rs.getString("ShelfNo");
+        String Block_Shelfno=rs.getString("Block_ShelfNo");
+        int row_no=rs.getInt("Shelf_row");
+        int col_no=rs.getInt("Shelf_col");
+        int OnShelf_ProQty=rs.getInt("OnShelf_qty");
+        String Stock_con=rs.getString("Stock_condition");
+        String Add_sts= rs.getString("Add_status");
+        ShelfManage sm =new ShelfManage(ProductID,ProductName,Block_no,Block_Shelfno,row_no,col_no,OnShelf_ProQty,Stock_con,Add_sts);
+        ShelfProductList.add(sm);
+         
+        }
+        return ShelfProductList;
+    }
+    
+
+    @FXML
+    private void AddQtyOnAction(ActionEvent event) {
+    }
+    
+    public static ShelfManage sm= new ShelfManage();
+    @FXML
+    private void updateProductOnAction(ActionEvent event) {
+        
+        
+        ObservableList<ShelfManage> selectedItem=FXCollections.observableArrayList();
+        selectedItem=Shelf_mngTable.getSelectionModel().getSelectedItems();
+         for (ShelfManage shelf_mng : selectedItem) {
+          sm.setProduct_Id(shelf_mng.Product_Id);
+          sm.setProduct_Name(shelf_mng.Product_Name);
+          sm.setBlock_no(shelf_mng.Block_no);
+          sm.setShelf_no(shelf_mng.Shelf_no);
+          sm.setShelf_row(shelf_mng.Shelf_row);
+          sm.setShelf_col(shelf_mng.Shelf_col);
+          sm.setOnShelf_quantity(shelf_mng.OnShelf_quantity);
+          sm.setStock_Con(shelf_mng.Stock_Con);
+          sm.setAdd_sts(shelf_mng.Add_sts);
+        }
+       
+      Parent pane;
+        try {
+            pane = FXMLLoader.load(getClass().getResource("Update_Product.fxml"));
+            tableAnc.getChildren().setAll(pane);
+        } catch (IOException ex) {
+            Logger.getLogger(Shelf_MngController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+             
+     
+    }
+
+    private void updateItemInfo(ObservableList<ShelfManage> selectedItem) {
+        
+    }
 }
+    
---
+++ src/grab/go/Update_ProductController.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grab.go;
+
+import DatabaseConnection.DBconnection;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author FUJITSU
+ */
+public class Update_ProductController implements Initializable {
+
+    @FXML
+    private JFXTextField Pro_id;
+    @FXML
+    private JFXTextField Pro_name;
+    @FXML
+    private JFXTextField Shelf_No;
+    @FXML
+    private JFXTextField Price;
+    @FXML
+    private Button Update_pro;
+    @FXML
+    private JFXComboBox<String> Block_No;
+    ObservableList<String> blocks = FXCollections.observableArrayList(
+                  "Block A","Block B","Block C","Block D","Block E"
+        );
+    @FXML
+    private JFXTextField Shelf_row;
+    @FXML
+    private JFXTextField Shelf_col;
+    @FXML
+    private AnchorPane updateProAnc;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        Block_No.setItems(blocks);
+        
+        String product_id=Shelf_MngController.sm.Product_Id;
+        String product_Name=Shelf_MngController.sm.Product_Name;
+        String blockNo=Shelf_MngController.sm.Block_no;
+        String shelfNo=Shelf_MngController.sm.Shelf_no;
+        int shelfRow=Shelf_MngController.sm.Shelf_row;
+        int shelfCol=Shelf_MngController.sm.Shelf_col;
+        
+        Pro_id.setText(product_id);
+        Pro_name.setText(product_Name);
+        Block_No.setValue(blockNo);
+        Shelf_No.setText(shelfNo);
+        Shelf_row.setText(Integer.toString(shelfRow));
+        Shelf_col.setText(Integer.toString(shelfCol));
+        
+        //unit_price from db
+        
+        String q="SELECT UnitPrice FROM Products where ProductID='"+product_id+"'";
+        DBconnection dbc =new DBconnection();
+        try { 
+            dbc.connectToDB();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Update_ProductController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Update_ProductController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        dbc.queryToDB(q);
+        ResultSet rs= dbc.queryToDB(q);
+        try {
+            if(rs.next()){
+                Price.setText(rs.getString("UnitPrice"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Update_ProductController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
+
+    @FXML
+    private void Update_productOnAction(ActionEvent event) throws ClassNotFoundException, SQLException, IOException {
+        DBconnection dbc = new DBconnection();
+        dbc.connectToDB();
+      String query = "UPDATE Shelf SET Block_shelfNo='"+Shelf_No.getText()+"', Shelf_row="+Shelf_row.getText()+", Shelf_col="+Shelf_col.getText()+" WHERE ProductID='"+Pro_id.getText()+"'";
+    
+       System.out.println(query);
+        //ResultSet dataInserted = dbc.queryToDB(query);
+         boolean dataIn = dbc.insertDataToDB(query);
+        System.out.println("Updated?: "+dataIn);
+        if(dataIn){
+            String q="UPDATE Products SET ShelfNo='"+Block_No.getValue()+"',UnitPrice= "+Price.getText()+" WHERE ProductID='"+Pro_id.getText()+"'";
+            boolean dataInserted=dbc.insertDataToDB(q);
+        
+            if(dataInserted){
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Success!");
+                alert.setHeaderText("Product Information updated");
+                //alert.setContentText(msg2);
+                alert.showAndWait();
+            
+                Parent pane=FXMLLoader.load(getClass().getResource("Shelf_Mng.fxml"));
+                updateProAnc.getChildren().setAll(pane);
+            }
+        }
+       // System.out.println("Inserted?: "+dataInserted);
+        
+    }
+    
+}

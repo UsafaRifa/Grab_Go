b14b5b702d66b44c92863ddf2e58c7b97cd63890
Usafa Rifa <usafarifa97@gmail.com>
2022-02-17 01:36:32 +0600

    Update

--- src/grab/go/Stock.java
+++ src/grab/go/Stock.java
@@ -4,13 +4,27 @@ package grab.go;
 
 public class Stock {
     
-    
+     String ProductID;
     String ProductName;
-   
     String ProductQuantity;
      String ProductUpcomingQuantity;
 
     public Stock() {
+    }
+
+    public Stock(String ProductID, String ProductName, String ProductQuantity, String ProductUpcomingQuantity) {
+        this.ProductID = ProductID;
+        this.ProductName = ProductName;
+        this.ProductQuantity = ProductQuantity;
+        this.ProductUpcomingQuantity = ProductUpcomingQuantity;
+    }
+
+    public String getProductID() {
+        return ProductID;
+    }
+
+    public void setProductID(String ProductID) {
+        this.ProductID = ProductID;
     }
 
     public String getProductName() {
@@ -37,12 +51,8 @@ public class Stock {
         this.ProductUpcomingQuantity = ProductUpcomingQuantity;
     }
 
-    public Stock(String ProductName, String ProductQuantity, String ProductUpcomingQuantity) {
-        this.ProductName = ProductName;
-        this.ProductQuantity = ProductQuantity;
-        this.ProductUpcomingQuantity = ProductUpcomingQuantity;
-    }
+    
 
-   
+    
     
 }
